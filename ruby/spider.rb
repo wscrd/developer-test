2a6187426c002dbc300c299c320349c6fdd53b2b
Lucas gomide <lgomide@uplexis.com.br>
2015-01-28 08:46:53 -0200

    add class spider

---
+++ ruby/spider.rb
@@ -0,0 +1,51 @@
+require "net/http"
+require "uri"
+
+class Spider
+   VERB_MAP = {
+    :get    => Net::HTTP::Get,
+    :post   => Net::HTTP::Post,
+    :put    => Net::HTTP::Put,
+    :delete => Net::HTTP::Delete
+  }
+
+  def initialize(endpoint = ENDPOINT)
+    uri = URI.parse(endpoint)
+    @http = Net::HTTP.new(uri.host, uri.port)
+  end
+
+  def get(path, params)
+    request :get, path, params
+  end
+
+  def post(path, params)
+    request :post, path, params
+  end
+
+  def put(path, params)
+    request :put, path, params
+  end
+
+  def delete(path, params)
+    request :delete, path, params
+  end
+
+  private
+  def request(method, path, params = {})
+    case method
+    when :get
+      full_path = encode_path_params(path, params)
+      request = VERB_MAP[method.to_sym].new(full_path)
+    else
+      request = VERB_MAP[method.to_sym].new(path)
+      request.set_form_data(params)
+    end
+
+    @http.request(request)
+  end
+
+  def encode_path_params(path, params)
+    encoded = URI.encode_www_form(params)
+    [path, encoded].join("?")
+  end
+end
